cfc1e10280d66144a8babc6e9da1d77eddb93f37
Ronald Paniagua <limitedj@gmail.com>
2023-04-14 10:34:01 -0400

    Open-jira-Seccion-7

---
+++ context/ui/UIProvider.tsx
@@ -0,0 +1,37 @@
+import { FC, useReducer, ReactNode, PropsWithChildren } from 'react';
+import { UIContext, uiReducer } from './';
+
+export interface UIState {
+    sidemenuOpen: boolean;
+    children?: PropsWithChildren;
+}
+
+
+const UI_INITIAL_STATE: UIState = {
+    sidemenuOpen: false,
+}
+
+
+export const UIProvider:FC<UIState> = ({ children }) => {
+
+    const [state, dispatch] = useReducer( uiReducer, UI_INITIAL_STATE );
+
+
+    const openSideMenu = () => {
+        dispatch({ type: 'UI - Open Sidebar' });
+    }
+
+    const closeSideMenu = () => dispatch({ type: 'UI - Close Sidebar' })
+
+    return (
+        <UIContext.Provider value={{
+            ...state,
+
+            // Methods
+            closeSideMenu,
+            openSideMenu,
+        }}>
+            { children }
+        </UIContext.Provider>
+    )
+};
